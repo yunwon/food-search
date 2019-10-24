2ecdd60010a6870191c1d0f45c79ef4a9bd63be4
jeongyun <jeongyunw@gmail.com>
2019-10-24 17:13:09 +1300

    first screen

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { StyleSheet, Text, View } from "react-native";
+import SearchScreen from "./src/screens/SearchScreen";
+
+const navigator = createStackNavigator(
+  {
+    Search: SearchScreen
+  },
+  {
+    initialRouteName: "Search",
+    defaultNavigationOptions: {
+      title: "Business Search"
+    }
+  }
+);
+
+export default createAppContainer(navigator);
