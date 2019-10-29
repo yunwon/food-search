d1f83904f8c755b2ae916cd62c75043ec36ca27a
jeongyun <jeongyunw@gmail.com>
2019-10-30 11:29:17 +1300

    added error handling

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -7,21 +7,27 @@ import yelp from "../api/Yelp";
 const SearchScreen = ({ navigation }) => {
   const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
+  const [errorMessage, seterrorMessage] = useState("");
 
   const searchApi = async () => {
-    const response = await yelp.get("/search", {
-      params: {
-        limit: 50,
-        term: term,
-        location: "auckland"
-      }
-    });
-    setResults(response.data.businesses);
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term: term,
+          location: "auckland"
+        }
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      seterrorMessage("Something went wrong 😢");
+    }
   };
 
   return (
     <View style={styles.container}>
       <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>{results.length} restaurants found </Text>
       <RestaurantList cost="Cost Effective" />
       <RestaurantList cost="Bit Pricer" />
